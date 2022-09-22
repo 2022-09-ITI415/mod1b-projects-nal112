f2bd6e0318b1c64c495873e23517adba3060ab1f
nal112 <113564610+nal112@users.noreply.github.com>
2022-09-22 14:19:38 -0400

    Added adding a high score script to the game.

--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // We need this line for uGUI to work
 
 public class HighScore : MonoBehaviour
 {
+    static public int score = 1000;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class HighScore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
     }
 }
